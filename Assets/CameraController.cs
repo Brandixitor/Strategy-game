99bb65c1a2eb44e1e52c04d59799994a78384d36
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-07 12:00:25 +0100

    Added mouse movement

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,12 +3,13 @@
 public class CameraController : MonoBehaviour
 {
     public float panSpeed = 20f;
+    public float panBoarderThickness = 10f;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
-        if (Input.GetKey("z"))
+        if (Input.GetKey("z") || Input.mousePosition.y >= Screen.height - panBoarderThickness)
         {
             pos.z += panSpeed * Time.deltaTime;
         }
