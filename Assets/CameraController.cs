ccce8e9d4a988d2b0dcc062066c0a7fba010a2c5
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-07 12:05:54 +0100

    Camera movement done

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -13,6 +13,22 @@ public class CameraController : MonoBehaviour
         {
             pos.z += panSpeed * Time.deltaTime;
         }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBoarderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBoarderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("q") || Input.mousePosition.x <= panBoarderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        
         transform.position = pos;
     }
 }
