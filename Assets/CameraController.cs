2eafbca668cd33cae3f6e7acb4a51c31d037ace8
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-07 17:29:12 +0100

    Update

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,7 +5,7 @@ public class CameraController : MonoBehaviour
     public float panSpeed = 20f;
     public float panBoarderThickness = 10f;
 
-    // Update is called once per frame
+    // Moving the camera using the mouse / ZSQD
     void Update()
     {
         Vector3 pos = transform.position;
