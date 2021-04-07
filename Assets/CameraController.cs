cb4e307681262b0f33aeea19408e7c6485f03199
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-07 11:54:01 +0100

    z axis added

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float panSpeed = 20f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        if (Input.GetKey("z"))
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
     }
 }
