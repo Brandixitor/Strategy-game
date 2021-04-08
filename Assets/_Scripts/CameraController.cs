00600fbc2c77ce58e000182bb2cca589b3920519
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-08 18:48:31 +0100

    Camera script

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBoarderThickness = 10f;
+    public Vector2 panLimit;
+    public float scrollSpeed = 20f;
+
+    // Moving the camera using the mouse / ZSQD
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey("z") || Input.mousePosition.y >= Screen.height - panBoarderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBoarderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBoarderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("q") || Input.mousePosition.x <= panBoarderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+
+          pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+          pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+        
+        transform.position = pos;
+
+    
+    }
+}
