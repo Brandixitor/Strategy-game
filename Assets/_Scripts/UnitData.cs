63529e98ac497b5bd84411a42db2f5931c0132f7
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-09 17:27:50 +0100

    V0.1

--- Assets/_Scripts/UnitData.cs
+++ Assets/_Scripts/UnitData.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 
 namespace LP.SG.Player.Units
 {
-    public class NewBehaviourScript : MonoBehaviour
+    [CreateAssetMenu(fileName = "New Unit", menuName = "New Unit")]
+    public class NewBehaviourScript : ScriptableObject
     {
+        public enum unitType
+        {
+           Villager,
+           Warrior,
+           TBA1,
+           TBA2
+        };
         
+        public bool isPlayerUnit;
+
+        public unitType type;
+
+        public new string name;
+
+        public GameObject unitPrefab;
+
+        public int cost;
+        public int attack;
+        public int health;
+        public int armor;
+
     }
 }
 
